500533a8dd0822ed381d5d12a61dc30ea8b8a742
LahiruV <lahiruvimukthi292@gmail.com>
2025-07-28 12:32:42 +1000

    feat: Add new destination images for the DestinationsPage

--- src/pages/DestinationsPage.tsx
+++ src/pages/DestinationsPage.tsx
@@ -41,44 +41,44 @@ const destinationIds = [
   },
   {
     id: 'unawatuna',
-    image: "https://plus.unsplash.com/premium_photo-1669748157617-a3a83cc8ea23?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image: "../../public/images/unawatuna.jpg",
   },
   {
     id: 'yala',
-    image: "https://images.unsplash.com/photo-1619183318129-cd95bc882275?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image: "../../public/images/yala.avif",
   },
   {
     id: 'polonnaruwa',
-    image: "https://images.unsplash.com/photo-1709729508706-87741ec2d50a?q=80&w=1925&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image: "../../public/images/polonnaruwa.avif",
   },
 
   {
     id: 'weligama',
-    image: "https://images.unsplash.com/photo-1520242279429-1f64b18816ef?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image: "../../public/images/weligama.avif",
   },
   {
     id: 'nuwaraeliya',
-    image: "https://images.unsplash.com/photo-1586193804147-64d5c02ef9c1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image: "../../public/images/nuwaraeliya.avif",
   },
   {
     id: 'rekawa',
-    image: "https://images.pexels.com/photos/847393/pexels-photo-847393.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    image: "../../public/images/rekawa.jpeg",
   },
   {
     id: 'galle',
-    image: "https://images.pexels.com/photos/27669343/pexels-photo-27669343/free-photo-of-a-building-with-arched-windows-and-arched-columns.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    image: "../../public/images/galle.jpg",
   },
   {
     id: 'arugambay',
-    image: "https://images.pexels.com/photos/1654489/pexels-photo-1654489.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    image: "../../public/images/arugambay.jpeg"
   },
   {
     id: 'colombo',
-    image: "https://images.unsplash.com/photo-1565135382393-c100c8b0a129?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image: "../../public/images/colombo.jpg",
   },
   {
     id: 'negombo',
-    image: "https://images.unsplash.com/photo-1561150018-8bb356679537?q=80&w=2044&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image: "../../public/images/negombo.avif",
   }
 ];
 
